3e11c354faad1a0f1be2dc795fdde46913d72265
Gastón García <gaston.garcia@gmail.com>
2019-11-07 13:23:37 -0600

    Adding initial commit. Running on Next and Tailwind

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Head from "next/head";
+import "../src/style.css";
+const Home = () => (
+  <div>
+    <p className="text-blue-900 text-xl">Hello Next.js</p>
+  </div>
+);
+
+export default Home;
